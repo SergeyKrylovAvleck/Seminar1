aacb139c1f32547b47ef5e3dfd9e1ea38d2beafb
Сергей <antiwirus@uandex.ru>
2023-05-28 12:24:38 +0300

    программа для определения большего числа из двух

--- z1/Program.cs
+++ z1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿Console.WriteLine("Введите первое число:");
+
+           int a = Convert.ToInt32(Console.ReadLine());
+
+           Console.WriteLine("Введите второе число:");
+
+           int b = Convert.ToInt32(Console.ReadLine());
+
+           Console.WriteLine(a > b ? "Первое число больше второго" : "Второе число больше первого");
